32dbdbf3a911d71b987c3b9bb38e56cc406778c9
albus9192631770 <pangbai@shanghaitech.edu.cn>
2021-01-04 01:37:24 +0800

    myagent structure finished

--- q_learning_agent.py
+++ q_learning_agent.py
@@ -1,7 +1,8 @@
+import random
 import numpy as np
 from math import ceil
 
-
+SCREEN_SIZE = (1920, 1080)
 dx, dy = 50, 50
 W, H = SCREEN_SIZE
 X, Y = ceil(W / dx), ceil(H / dy)
@@ -13,7 +14,7 @@ def relative_pos(a, b):
 
 class ModelFreeReinforcementLearningAgent:
     def __init__(self, gamma, **kwargs):
-        self.gamma = kwargs['gamma']
+        self.gamma = gamma
         
     def decide(self, s):
         abstract
@@ -36,23 +37,35 @@ class ModelFreeReinforcementLearningAgent:
     def get_V_opt_a(self, s):
         actions = self.get_possible_actions(s)
         if 0 == len(actions):
-            input('Warning! No possible action!')
+            input('Warning! No possible action!')  
+            # TODO: Need to do something when nothing is given.
         return max([(self.get_Q(s,a),a) for a in actions])
 
     def get_V(self, s):
         return get_V_opt_a(self, s)[0]
 
-    def __get_optimal_action(self, s):
+    def get_optimal_action(self, s):
         return get_V_opt_a(self, s)[1]
     
 
 class EpsilonGreedyAgent(ModelFreeReinforcementLearningAgent):
     def __init__(self, epsilon, **kwargs):
         super().__init__(**kwargs)
-        self.epsilon = kwargs['epsilon']
+        self.epsilon = epsilon
 
     def decide(self, s):
-        pass
+        if random.random() < self.epsilon:
+            return self.get_random_action(s)
+        else:
+            return self.get_optimal_action(s)
+
+    def get_random_action(s):
+        actions = self.get_possible_actions(s)
+        if 0 == len(actions):
+            input('Warning! No possible action!')  
+            # TODO: Need to do something when nothing is given.
+        return random.sample(actions, 1)
+
 
 class SoftmaxAgent(ModelFreeReinforcementLearningAgent):
     def __init__(self, epsilon, **kwargs):
@@ -65,7 +78,7 @@ class SoftmaxAgent(ModelFreeReinforcementLearningAgent):
 class ClassicalQLearningAgent(ModelFreeReinforcementLearningAgent):
     def __init__(self, alpha, **kwargs):
         super().__init__(**kwargs)
-        self.alpha = kwargs['alpha']
+        self.alpha = alpha
         self.Q = np.zeros((X,Y,T))
    
     def get_Q(self, s, a):
@@ -75,14 +88,14 @@ class ClassicalQLearningAgent(ModelFreeReinforcementLearningAgent):
     def update_Q(self, s, a, s_, R):
         (x, y), t = a
         Q_ = R + self.gamma * self.get_V(s_)
-        diff = Q_ - self.self.Q[x, y, t]
+        diff = Q_ - self.Q[x, y, t]
         self.Q[x, y, t] += (self.alpha * diff)
 
 
 class DeepQlearningAgent(ModelFreeReinforcementLearningAgent):
     def __init__(self, alpha, **kwargs):
         super().__init__(**kwargs)
-        self.alpha = kwargs['alpha']
+        self.alpha = alpha
         self.Q = None
 
     def get_Q(self, s, a):
@@ -92,6 +105,70 @@ class DeepQlearningAgent(ModelFreeReinforcementLearningAgent):
         pass
 
 
-class MyAgent(EpsilonGreedyAgent, ClassicalQLearningAgent):
-    def __init__(self, alpha=2345, gamma=2134):
-        pass
+# NOTE: Testing in Programming Assignment ONLY!!!!!!!!!!!!!!!!!
+class QLearningAgentDemo(ModelFreeReinforcementLearningAgent, ReinforcementAgent):
+    def __init__(self, alpha, **kwargs):
+        super().__init__(**kwargs)
+        self.alpha = alpha
+        self.Q = util.Counter()
+   
+    def get_Q(self, s, a):
+        if not a in self.Q.keys():
+            self.Q[s, a] = 0
+        return self.Q[s, a]
+
+    def update_Q(self, s, a, s_, R):
+        Q_ = R + self.gamma * self.get_V(s_)
+        diff = Q_ - self.Q[s, a]
+        self.Q[s, a] += (self.alpha * diff)
+        
+    def get_V_opt_a(self, s):
+        actions = self.get_possible_actions(s)
+        if 0 == len(actions):
+            return (0, None)
+        return max([(self.get_Q(s,a),a) for a in actions])
+
+    def get_possible_actions(self, s):
+        return ReinforcementAgent.getLegalActions(self, s)
+    
+    def get_random_action(s):
+        actions = self.get_possible_actions(s)
+        if 0 == len(actions):
+            return None
+        return random.sample(actions, 1)    
+        
+
+# NOTE: Testing in Programming Assignment ONLY!!!!!!!!!!!!!!!!!
+class MyAgent(EpsilonGreedyAgent, QLearningAgentDemo, ReinforcementAgent):
+    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
+        args['epsilon'] = epsilon
+        args['gamma'] = gamma
+        args['alpha'] = alpha
+        self.numTraining = numTraining
+        self.index = 0  # This is always Pacman
+        EpsilonGreedyAgent.__init__(self, **args)
+        ClassicalQLearningAgent.__init__(self, **args)
+        ReinforcementAgent.__init__(self, **args)
+
+    def getQvalue(self, s, a):
+        return self.get_Q(s, a)
+
+    def computeValueFromQValues(self, s):
+        return self.get_V(s)
+
+    def computeActionFromQValues(self, s):
+        return self.get_optimal_action(s)
+
+    def getAction(self, s):
+        return self.decide(s)
+
+    def update(self, s, a, s_, R):
+        self.update_Q(s, a, s_, R)
+
+    def getPolicy(self, state):
+        return self.computeActionFromQValues(state)
+
+    def getValue(self, state):
+        return self.computeValueFromQValues(state)
+
+    
