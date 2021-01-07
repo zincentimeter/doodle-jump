260eb26323146036ac38813dbd39b5c12162c12c
soundwich <pangbai@shanghaitech.edu.cn>
2021-01-08 00:01:55 +0800

    DQN implemented

--- agent_base_classes.py
+++ agent_base_classes.py
@@ -2,12 +2,14 @@ import random
 import numpy as np
 from math import ceil, exp, trunc
 from utils import *
+from collections import namedtuple
 
 
 try:
     del abstract, not_defined
 except:
     pass
+
 
 class ModelFreeReinforcementLearningAgent:
     def __init__(self, gamma, **kwargs):
@@ -22,8 +24,8 @@ class ModelFreeReinforcementLearningAgent:
     def get_Q(self, s, a):
         abstract
 
-    def update_Q(self, s, a, s_, R):
-        abstract
+    def get_Q_a_list(self, s):
+        abstact
 
     def get_possible_actions(self, s):  # inplemented in Interface
         abstract
@@ -33,10 +35,7 @@ class ModelFreeReinforcementLearningAgent:
         # TODO: Need to do something when nothing is given.
 
     def get_V_opt_a(self, s):
-        actions = self.get_possible_actions(s)
-        if 0 == len(actions):
-            self.raise_no_possible_actions_error()
-        return max([(self.get_Q(s, a), a) for a in actions])
+        return max(self.get_Q_a_list(s))
 
     def get_V(self, s):
         return self.get_V_opt_a(s)[0]
@@ -46,10 +45,15 @@ class ModelFreeReinforcementLearningAgent:
 
 MFRLA = ModelFreeReinforcementLearningAgent
 
+
 class EpsilonGreedyAgent(MFRLA):
-    def __init__(self, epsilon, **kwargs):
+    def __init__(self, epsilon, eps_decay=None, eps_end=None, **kwargs):
         super().__init__(**kwargs)
         self.epsilon = epsilon
+        if eps_decay:
+            self.eps_decay = eps_decay
+        if eps_end:
+            self.eps_end = eps_end
 
     def decide(self, s):
         if random.random() < self.epsilon:
@@ -65,12 +69,14 @@ class EpsilonGreedyAgent(MFRLA):
 
 EGA = EpsilonGreedyAgent
 
+
 class SoftmaxAgent(MFRLA):
     def decide(self, s):
         actions = self.get_possible_actions(s)
         if 0 == len(actions):
             self.raise_no_possible_actions_error()
-        dist = [(exp(self.get_Q(s, a)), a) for a in actions]
+        dist = self.get_Q_a_list(s)
+        dist = [(exp(Q), a) for Q, a in dist]
         return self.__choose_by_weight(dist)
 
     def __choose_by_weight(self, dist):
@@ -92,6 +98,7 @@ class SoftmaxAgent(MFRLA):
 
 SMA = SoftmaxAgent
 
+
 class QLearningAgent(MFRLA):
     def __init__(self, alpha, **kwargs):
         super().__init__(**kwargs)
@@ -99,8 +106,9 @@ class QLearningAgent(MFRLA):
         self.Q = dict()
 
     def observe(self, s, a, s_, R):
-        self.update_Q(s, a, s_, R)
-        return self.Q
+        Q_sample = R + self.gamma * self.get_V(s_)
+        diff = Q_sample - self.get_Q(s, a)
+        self.Q[s, a] += (self.alpha * diff)
 
     def get_Q(self, s, a):
         try:
@@ -110,12 +118,14 @@ class QLearningAgent(MFRLA):
             self.Q[s, a] = default
             return default
 
-    def update_Q(self, s, a, s_, R):
-        Q_sample = R + self.gamma * self.get_V(s_)
-        diff = Q_sample - self.get_Q(s, a)
-        self.Q[s, a] += (self.alpha * diff)
+    def get_Q_a_list(self, s):
+        actions = self.get_possible_actions(s)
+        if 0 == len(actions):
+            self.raise_no_possible_actions_error()
+        return [(self.get_Q(s, a), a) for a in actions]
 
 QLA = QLearningAgent
+
 
 ########################
 # Deep Q Learning Part #
@@ -127,6 +137,7 @@ import torch.nn.functional as F
 import torchvision.transforms as T
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class DeepQNetwork(nn.Module):
     # def __init__(self, h, w, outputs):
@@ -143,21 +154,20 @@ class DeepQNetwork(nn.Module):
     #     x = F.relu(self.bn2(self.conv2(x)))
     #     x = F.relu(self.bn3(self.conv3(x)))
     #     return self.head(x.view(x.size(0), -1))
-    def __init__(self, dim_state=4*30, dim_action=2):
+    def __init__(self, dim_state, dim_action):
         super(DeepQNetwork, self).__init__()
-        self.fc1 = nn.Linear(dim_state, 50)
+        self.fc1 = nn.Linear(dim_state, 60)
         self.fc1.weight.data.normal_(0, 0.1)
-        # self.fc2 = nn.Linear(50, 50)
-        # self.fc2.weight.data.normal_(0, 0.1)
-        self.out = nn.Linear(50, dim_action)
+        self.fc2 = nn.Linear(60, 30)
+        self.fc2.weight.data.normal_(0, 0.1)
+        self.out = nn.Linear(30, dim_action)
         self.out.weight.data.normal_(0, 0.1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
+        x = F.relu(self.fc2(x))
         x = F.relu(self.out(x))
         return x
-
 
 
 class ReplayMemory:
@@ -176,66 +186,62 @@ class ReplayMemory:
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
 
-    def is_full(self):
-        return len(self.memory) == self.capacity
+    def __len__(self):
+        return len(self.memory)
 
 
-class DeepQLearningAgent(MFRLA):
-    def __init__(self, alpha, **kwargs):
-        super().__init__(**kwargs)
-        self.alpha = alpha
-        self.Q = DeepQNetwork().to(DEVICE)
+Transition = namedtuple('Transition', ('s', 'a', 's_', 'R'))
 
-    def learn(self):
-        abstract
-
-    def get_Q(self, s, a):
-        return self.Q.query(s, a)
-
-        
-DQLA = DeepQLearningAgent
-
-class NaiveDeepQLearningAgent(DQLA):
-    def observe(self, s, a, s_, R):
-        self.update_Q(s, a, s_, R)
-
-    def update_Q(self, s, a, s_, R):
-        self.Q.update(s, a, s_, R)
-
-NDQLA = NaiveDeepQLearningAgent
 
 class ExperienceReplayAgent(DQLA):
-    def __init__(self, **kwargs):
+    def __init__(self, alpha, C, batch_size, dim_state=4*10*3, dim_action=2, **kwargs):
         super().__init__(**kwargs)
-        self.exp = self.ReplayMemory(10000)
-
-    def observe(self, s, a, s_, R):
-        transition = (s, a, s_, R)
-        self.exp.push(transition)
-
-    def learn(self):
-        not_defined  # TODO: not_defined
-        
-    def update_Q(self, s, a, s_, R):
-        not_defined  # TODO: not_defined
-        
-ERA = ExperienceReplayAgent
-    
-class FixedQTargetsAgent(DQLA):
-    def __init__(self, C, **kwargs):
-        super().__init__(**kwargs)
-        self.C = C  # Every C step, set Q_target = Q
+        self.alpha = alpha
+        self.Q = DeepQNetwork(dim_state, dim_action).to(DEVICE)
         self.step_counter = 0  
-        self.Q_target = DeepQNetwork().to(DEVICE)
-        self.Q_target.load_state_dict(self.Q.state_dict()).eval()
+        self.C = C  # Every C step, set Q_target = Q
+        self.batch_size = batch_size
+        self.Q_target = DeepQNetwork(dim_state, dim_action).to(DEVICE)
+        self.Q_target.load_state_dict(self.Q.state_dict())
+        self.Q_target.eval()
+        self.exp = ReplayMemory(10000)
+        self.optimizer = optim.RMSprop(self.Q.parameters())
 
-    def observe(self, s, a, s_, R):  # save experience
-        not_defined  # TODO: not_defined
-    
+    def enter_new_episode(self):
+        self.step_counter += 1
+        if not (self.step_counter % self.C):
+            self.Q_target.load_state_dict(self.Q.state_dict())
+        
+    def observe(self, s, a, s_, R):
+        transition = Transition(s, a, s_, R)
+        self.exp.push(transition)
+        self.learn()
+
     def learn(self):
-        not_defined  # TODO: not_defined
+        if len(self.exp) < self.batch_size:
+            return
+        transitions = self.exp.sample(self.batch_size)
+        minibatch = Transition(*zip(*transitions))
+        s = torch.cat(minibatch.s)
+        a = torch.cat(minibatch.a)
+        s_ = torch.cat(minibatch.s_)
+        R = torch.cat(minibatch.R)
+        Q = self.Q(s).gather(1, a)
+        V_s_ = self.Q_target(s_).max(1)[0].detach()
+        Q_sample = (R + self.gamma * V_s_).unsqueezze(1)
+        loss = F.smooth_l1_loss(Q_sample, Q)
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.Q.parameters():
+            param.grad.data.clamp(-1,1)
+        self.optimizer.step()
 
-    def update_Q(self, s, a, s_, R):
-        not_defined  # TODO: not_defined
+    def get_Q(self, s, a):
+        return self.get_Q_a_list(s)[a][0]
 
-FQTA = FixedQTargetsAgent
+    def get_Q_a_list(self, s):
+        with torch.no_grad():
+            y = self.Q(s)
+        return [(q.item(), i) for i, q in enumerate(y[0])]
+
+ERA = ExperienceReplayAgent
